62a0e1ce64885a128a18ca25b64feca37dfa2c29
Tramy Nguyen <tramynn2@gmail.com>
2019-09-19 09:56:36 -0500

    server setup

---
+++ server/controllers/authController.js
@@ -0,0 +1,6 @@
+module.exports = {
+  getUser: (req, res) => {},
+  register: (req, res) => {},
+  login: (req, res) => {},
+  logout: (req, res) => {}
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const { SERVER_PORT } = process.env;
+
+// auth endpoints
+app.get("/auth/user");
+app.post("/auth/register");
+app.post("/auth/login");
+app.post("/auth/logout");
+
+// forum endpoints
+
+// post endpoints
+app.get("/api/forum");
+app.get("/api/posts/:topic");
+app.post("/api/posts/:topic");
+app.delete("/api/posts/:id");
 
 app.listen(SERVER_PORT, () => {
-  
-})
+  console.log(`Server listening on PORT: ${SERVER_PORT}`);
+});
